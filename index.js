8b9bc5455776be3e733fa16dd115195bb1755bb4
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-21 20:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,3 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
-
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
-  delete newObj.key
-  return object
-}
